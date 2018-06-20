b3c535e23752b65c29038e09d9d3ae9163bcc85c
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-06-20 16:23:19 +0000

    initilize collapsible materilize item and check if user was signed in before actions on post

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,12 +1,17 @@
 $(document).on('turbolinks:load', function() {
   $('.sidenav').sidenav();
   $('.slider').slider({
-    height: 600
-  });
-  closeOnClick: true
+    height: 600,
+    closeOnClick: true
+  });  
+  $('.collapsible').collapsible();
+  
+ 
 });
 
 document.addEventListener("turbolinks:before-cache", function() {
   $('.sidenav').sidenav('destroy');
   $('.slider').slider('destroy');
+  $('.collapsible').collapsible('destroy');
+ 
 });
