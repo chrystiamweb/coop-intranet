793960905599285e9e2a59e21547a2b220d11539
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-07-03 12:56:18 +0000

    edit sidenav reordering items

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ puts "usuário padrão Criado "
 puts "Cirando usuários aleatórios"
 
 
-250.times do 
+50.times do 
     User.create(  
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
