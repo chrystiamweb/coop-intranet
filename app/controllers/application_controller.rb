d8bff8871009aedbc307fe7b7a8c3a5b321d0786
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-07-17 20:49:18 +0000

    included kind filed into devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
-class ApplicationController < ActionController::Base    
+class ApplicationController < ActionController::Base 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
     def home
     end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:kind])
+    end 
 end
