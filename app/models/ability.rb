75adedd7ebebe3c2d6c79795a7817a49e8bda8a6
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-08-13 16:29:37 +0000

    changed ability to normal user calc taxes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       if user.admin?
         can :manage, :all
       else 
-        can :read, :allS        
+        can :read, :all
       end
     #
     # The first argument to `can` is the action you are giving the user
