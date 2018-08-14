b58596b28e3dbc128980639ecfa6209cced4867d
Chrystiam Monteiro Andrade <ChrystiamM3047_00@credicope.local>
2018-08-14 17:49:25 -0300

    changed post exhibition order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-     @lastposts = Post.last(5)
-     @lastnormatives = Normative.last(5)     
+     @lastposts = Post.first(5)
+     @lastnormatives = Normative.first(5)     
      @birth_days_by_month = User.where("EXTRACT(MONTH FROM birthday) = ?", Time.zone.now.month).order("extract(day from birthday) ASC")
      @birth_days_by_day = @birth_days_by_month.where("EXTRACT(DAY FROM birthday)= ?", Time.zone.now.day).order(:birthday)
     end
