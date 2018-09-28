be8b750d8a06defb210853debc49e6b1c0dfa4b0
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-09-28 12:27:51 +0000

    Deploy db task capristrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,3 +43,16 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+namespace :db do
+    desc 'Resets DB without create/drop'
+    task :reset do
+      on primary :db do
+        within release_path do
+          with rails_env: fetch(:stage) do
+            execute :rake, 'db:schema:load'
+            execute :rake, 'db:seed'
+          end
+        end
+      end
+    end
+  end
