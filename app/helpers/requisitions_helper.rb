4d436261d02f039961c2820cb9398c6237952f79
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-04-08 17:06:01 -0300

    Calc status per hour

--- app/helpers/requisitions_helper.rb
+++ app/helpers/requisitions_helper.rb
@@ -1,2 +1,22 @@
 module RequisitionsHelper
+    def time_req_calc
+            status = StatusAction.closed.select("sector_flow_id").distinct 
+            status_time_calc = []
+            media_calc = 0
+            status.each do |stat|                
+                medias = StatusAction.where(sector_flow_id: stat.sector_flow_id).closed
+                medias.each do |m|
+                    media_calc +=  diff_time(m.start,m.finish)
+                end
+               media_calc /= medias.count
+                puts [stat.sector_flow.name, media_calc]
+                status_time_calc << [stat.sector_flow.name,media_calc]
+            end
+            puts "STATUS TIME TEST"
+            status_time_calc
+    end
+
+    def diff_time(start,finish)       
+        ((finish - start) / 1.hour).round
+    end
 end
--- app/models/status_action.rb
+++ app/models/status_action.rb
@@ -2,4 +2,6 @@ class StatusAction < ApplicationRecord
   belongs_to :requisition
   belongs_to :sector_flow
   belongs_to :requisition_status
+
+  scope :closed , -> {where.not(finish: nil)}
 end
