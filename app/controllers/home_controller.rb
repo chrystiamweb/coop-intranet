c6d589b62615e2c49dc4ee95a43405e719f208fb
Chrystiam Monteiro Andrade <ChrystiamM3047_00@credicope.local>
2019-04-12 10:37:01 -0300

    add feature to import clients from CSV

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,20 @@
 class HomeController < ApplicationController
-    def index
-     @lastposts = Post.order(created_at: :desc).first(5)
-     @lastnormatives = Normative.first(5)     
-     @birth_days_by_month = User.where("EXTRACT(MONTH FROM birthday) = ?", Time.zone.now.month).order("extract(day from birthday) ASC")
-     @birth_days_by_day = @birth_days_by_month.where("EXTRACT(DAY FROM birthday)= ?", Time.zone.now.day).order(:birthday)
-    end
+	def index
+		@lastposts = Post.order(created_at: :desc).first(5)
+		@lastnormatives = Normative.first(5)     
+		@birth_days_by_month = User.where("EXTRACT(MONTH FROM birthday) = ?", Time.zone.now.month).order("extract(day from birthday) ASC")
+		@birth_days_by_day = @birth_days_by_month.where("EXTRACT(DAY FROM birthday)= ?", Time.zone.now.day).order(:birthday)
+	end
 
-    def goals
-        
-    end
+	def goals        
+	end
+
+	def import
+		Client.import(params[:file])
+		redirect_to root_url, notice: "Usuários importador com sucesso!"
+	end
+
+	def configs
+	end
     
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,22 @@
 class Client < ApplicationRecord
+  require 'csv'
   belongs_to :location
   validates :name, presence: true
   validates :cpfcnpj, presence: true, length: { maximum: 14 }
+
+  def self.import(file)
+    csv_text = File.read(file)
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |p|
+      Client.where(cpfcnpj: p["Número CPF/CNPJ"]).first_or_create do |client|
+        client.name = p["Nome Cliente"]
+        client.cpfcnpj = p["Número CPF/CNPJ"]
+        client.income_type = p["Tipo de Renda"]
+        client.category = p["Sigla Tipo Pessoa"]
+        client.rating = p["Descrição Nivel Risco CRL"]
+        client.location = Location.where(name:  p['Número PA'].length < 2 ? "PA0#{p['Número PA']}" : "PA#{p['Número PA']}").first
+      end
+    end
+  end
+  
 end
