3cc798f8ffe9e5fae176163f5239c6d66c371aed
Chrystiam Monteiro Andrade <ChrystiamM3047_00@credicope.local>
2019-04-15 16:28:11 -0300

    create scopes according to locations

--- app/models/goals_report.rb
+++ app/models/goals_report.rb
@@ -3,12 +3,23 @@ class GoalsReport < ApplicationRecord
   belongs_to :location
   has_one_attached :report_img
 
+  def self.same_location(location)
+    if location.name == 'PA00'
+      self.by_last_location
+    else 
+    self.where(location: location).last
+    end
+  end
+  def self.by_last_location
+    self.where(location: Location.last).last
+  end
+
   def self.import(files)
     files.each do |i|
       temp = GoalsReport.new(
         name:  i.original_filename,
         goal_category_id: 1,
-        location_id: 1,
+        location_id: Location.find_by_number(i.original_filename.split('-')[0]).first.id,
       )   
       temp.report_img.attach(
         io: File.open(i.path), 
