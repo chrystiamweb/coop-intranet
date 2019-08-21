900bd40ac37351ba9a460d1b2d1934d6c6d98479
Chrystiam Monteiro Andrade <ChrystiamM3047_00@credicope.local>
2019-08-21 11:54:32 -0300

    update to rails 6

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,9 @@
 Devise.setup do |config|
-  #Rails.env.production?  do
+  Rails.env.production?  do
     config.warden do |manager|
       manager.default_strategies(:scope => :user).unshift :ldap_authenticatable
     end
-  #end
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
