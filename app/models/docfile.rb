0fa48c59ba7a938c058a47ab21d3315e989e00c6
Chrystiam Monteiro Andrade <chrystiamm3047_00@credicope.com>
2020-07-14 15:16:36 -0300

    split payroll pdf item

--- app/models/docfile.rb
+++ app/models/docfile.rb
@@ -1,4 +1,25 @@
 class Docfile < ApplicationRecord
   has_one_attached :doc_file
   belongs_to :file_type
+
+  def self.splitPayroll(payroll)
+    reader = PDF::Reader.new(payroll.tempfile)    
+    reader.pages.each_with_index do |page,i|
+      data = page.raw_content.scan(/\(.*?\)/)
+      pdf_name =   "#{data[15].gsub(/[()]/, "")}"
+      pdf = CombinePDF.new
+      pdf << CombinePDF.load(payroll.tempfile.path).pages[i]
+      pdf.save "#{pdf_name}.pdf"  
+      temp = Docfile.new(
+        name:  "#{pdf_name} - #{Date.current}",
+        file_type_id: 5,
+      )   
+      temp.doc_file.attach(
+        io: File.open("#{pdf_name}.pdf"),
+        filename:"#{pdf_name} - #{Date.current}.pdf" 
+      )          
+      temp.save!
+      File.delete("#{pdf_name}.pdf")  
+    end    
+  end
 end
